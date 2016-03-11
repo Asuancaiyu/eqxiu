73533c0d152e7952504700cb261b9b6184e85ebb
ytftianwen <2419214712@qq.com>
2016-03-11 19:06:27 +0800

    Initial commit

---
+++ Conf/db.php
@@ -0,0 +1,12 @@
+<?php
+return array (
+  'DB_TYPE' => 'mysql',
+  'DB_HOST' => 'localhost',
+  'DB_PORT' => '3306',
+  'DB_NAME' => 'yiqixiu',
+  'DB_USER' => 'root',
+  'DB_PWD' => '',
+  'DB_PREFIX' =>' cj_',
+  'agent_version' => 0,
+);
+?>
